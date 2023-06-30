77eafa57f73dccd41e6e8ba610e44b5ab5b43c92
Sagar@6205 <sagarraj143a@gmail.com>
2023-06-30 23:53:25 +0530

    Remove Element

---
+++ Array/Array-09.cpp
@@ -0,0 +1,37 @@
+// 27. Remove Element (LeetCode)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0, s = 0;
+        while (i < nums.size()) {
+            if (nums[i] != val) {
+                nums[s] = nums[i];
+                s++;
+            }
+            i++;
+        }
+        return s;
+    }
+};
+
+int main() {
+    vector<int> nums;
+    nums.push_back(3);
+    nums.push_back(2);
+    nums.push_back(2);
+    nums.push_back(3);
+    
+    int val = 3;
+    
+    Solution solution;
+    int Size = solution.removeElement(nums, val);
+    
+    cout << Size << endl;
+    
+    return 0;
+}
