e2fc8c5ea244991bdf6ba814e8ad9365b31972fa
Sagar@6205 <sagarraj143a@gmail.com>
2023-07-04 12:22:07 +0530

    Length Of Last Word

---
+++ Array/Array-14.cpp
@@ -0,0 +1,22 @@
+// 58 . Length Of Last Word
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+      int n=s.size()-1;
+      int count =0;
+      
+      while(s[n]==' '){
+          n--;
+          
+      }
+      
+      while(n>=0 && s[n]!=' '){
+          count++;
+          n--;
+      }
+      return count;
+    }
+};
