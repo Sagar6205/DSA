587442faae7900ccad8b59add4070af42157a1d0
Sagar@6205 <sagarraj143a@gmail.com>
2023-07-06 00:35:01 +0530

    Two Sum (LeetCode)

---
+++ Array/Array-16.cpp
@@ -0,0 +1,17 @@
+// 1. Two Sum (LeetCode)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i=0,j=0;
+        
+        for(i=0;i<nums.size();i++){
+            for(j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                   return {i,j};
+                }
+            }
+        }
+        
+      return {i,j};  
+    }
+};
